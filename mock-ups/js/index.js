475d1de93e52bc7a27e4888545f3e43ecb4090d5
Sunday Power Inemesit <sundaypower9@gmail.com>
2019-02-08 14:42:50 +0100

    Add files via upload

--- mock-ups/js/index.js
+++ mock-ups/js/index.js
@@ -2,12 +2,12 @@
 this.addEventListener('DOMContentLoaded', function()
 {
   //set height of landing page image on page load
-  S('.cur-page-bg-image').height = `${this.innerHeight}px`;
+  S('.cur-page-top-bg-image').height = `${this.innerHeight}px`;
 
 
   
   this.addEventListener("resize", () =>
   {
-    S('.cur-page-bg-image').height = `${window.innerHeight}px`;
+    S('.cur-page-top-bg-image').height = `${window.innerHeight}px`;
   });
 });
